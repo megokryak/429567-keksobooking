1ad914b7bf1403bb5d9b53ff5a402192dcd1deaf
Andrey <XaJIaC@yandex.ru>
2018-07-09 20:04:26 +0300

    Выполнил первую часть дз

--- js/get-random.js
+++ js/get-random.js
@@ -3,10 +3,10 @@
   window.getRandom = {
     getRandomArray: function (arr) {
       var newArr = arr.slice(0);
-      for (var k = newArr.length - 1; k > 0; k--) {
-        var j = Math.floor(Math.random() * (k + 1));
-        var temp = newArr[k];
-        newArr[k] = newArr[j];
+      for (var i = newArr.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = newArr[i];
+        newArr[i] = newArr[j];
         newArr[j] = temp;
       }
       return newArr;
