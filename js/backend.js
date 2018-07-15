ab72faba64e9a8c1b3ad1de0e737a1c11cb8c076
Andrey <XaJIaC@yandex.ru>
2018-07-15 17:29:17 +0300

    nebolwou promax

---
+++ js/backend.js
@@ -0,0 +1,56 @@
+(function() {
+  window.backend = {
+    load: function (onLoad, onError) {
+      var URL = ' https://js.dump.academy/keksobooking/data';
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      xhr.addEventListener ('load', function () {
+        switch (xhr.status) {
+          case 200:
+            onLoad(xhr.response);
+            break;
+          case 400:
+            onError('Ошибка 4хх');
+            break;
+          case 300:
+            onError('Косяк! Перенаправление. Ошибка 3хх');
+            break;
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Ошибка сервера');
+      });
+      xhr.addEventListener('timeOut', function () {
+        onError('Вышло время загрузки');
+      });
+      xhr.open('GET', URL);
+      xhr.send();
+    },
+    save: function () {
+      var URLPost = 'https://js.dump.academy/keksobooking';
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      xhr.addEventListener('load', function () {
+        switch (xhr.status) {
+          case 200:
+            onLoad(xhr.response);
+            break;
+          case 400:
+            onError('Ошибка 4хх');
+            break;
+          case 300:
+            onError('Косяк! Перенаправление. Ошибка 3хх');
+            break;
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Ошибка сервера');
+      });
+      xhr.addEventListener('timeOut', function () {
+        onError('Вышло время загрузки');
+      });
+      xhr.open('POST', URLPost);
+      xhr.send(data);
+    }
+  }
+})();
